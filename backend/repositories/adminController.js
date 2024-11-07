da99e51023a08c69332d91c75b5f71aa861c05ce
Martin <martinberg9@proton.me>
2024-11-07 07:52:52 +0100

    edit get request for getting all user data

--- backend/repositories/adminController.js
+++ backend/repositories/adminController.js
@@ -2,7 +2,13 @@ const db = require('../utils');
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await db.query('SELECT * FROM app_user');
+    const users = await db.query(`
+      SELECT
+        app_user.id, app_user.role, app_user.first_name, app_user.last_name,
+        app_user.email_address, app_user.password, app_user.date_of_birth, app_user.address
+      FROM app_user
+      LEFT JOIN address ON app_user.address = address.id
+      `);
     return res.status(200).json(users.rows);
   } catch (error) {
     console.error(error);
