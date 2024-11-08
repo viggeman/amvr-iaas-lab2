aab7e4bd8f45944de6929584d5d9e5aa61a0a150
Martin <martinberg9@proton.me>
2024-11-08 13:30:55 +0100

    add modifyuser scaffold, install react hook form

--- frontend/src/pages/admin/AdminModify.tsx
+++ frontend/src/pages/admin/AdminModify.tsx
@@ -28,6 +28,43 @@ const AdminModify = () => {
   return (
     <div className={styles.main}>
       <h2>Adminmodify</h2>
+      {result !== null &&
+        result.map((user) => (
+          <div key={user.id} className={styles.user}>
+            <p>
+              <strong>Id: </strong>
+              {user.id}
+            </p>
+            <p>
+              <strong>Role: </strong>
+              {user.role}
+            </p>
+            <p>
+              <strong>First Name: </strong>
+              {user.first_name}
+            </p>
+            <p>
+              <strong>Last Name: </strong>
+              {user.last_name}
+            </p>
+            <p>
+              <strong>Email address: </strong>
+              {user.email_address}
+            </p>
+            <p>
+              <strong>Password: </strong>
+              {user.password}
+            </p>
+            <p>
+              <strong>Date of birth: </strong>
+              {user.date_of_birth}
+            </p>
+            <p>
+              <strong>AddressId: </strong>
+              {user.address ?? 'N/A'}
+            </p>
+          </div>
+        ))}
     </div>
   );
 };
