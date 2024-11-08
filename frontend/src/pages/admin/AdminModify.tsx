beae0b5726abdb3ddf146e9eb90759b327adb5d2
Martin <martinberg9@proton.me>
2024-11-08 12:59:00 +0100

    add working fetch in adminmodify component

--- frontend/src/pages/admin/AdminModify.tsx
+++ frontend/src/pages/admin/AdminModify.tsx
@@ -15,19 +15,17 @@ interface Users {
 
 const AdminModify = () => {
   const [result, setResult] = useState<null | Users[]>(null);
-
   const { userId } = useParams();
-  console.log(userId);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`/api/admin`);
+        const response = await fetch(`/api/admin/get-user/${userId}`);
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        console.log(data);
+        console.log('Data:', data);
         setResult(data);
       } catch (error) {
         console.error(error);
@@ -35,7 +33,7 @@ const AdminModify = () => {
     };
 
     fetchData();
-  }, []);
+  }, [userId]);
 
   return (
     <div className={styles.main}>
